60afe533754ac404d380c27dd31854654349d78b
Divya Rasania <70239770+DivyaRasania@users.noreply.github.com>
2023-07-06 19:59:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const change = () => {
+    let hexNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E"];
+    let hexCode = "";
+    for (let i = 0; i < 6; i++) {
+        let randomIndex = Math.floor(Math.random() * hexNumbers.length);
+        hexCode += hexNumbers[randomIndex];
+    }
+
+    document.getElementById("hex-code").innerHTML = hexCode;
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#" + hexCode;
+}
